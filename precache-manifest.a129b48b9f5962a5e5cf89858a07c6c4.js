94aac9917a61cbb57fb06b99f1efcf70b551b7fa
Vyshniakov Ihor <ihor.vyshniakov@qlicks.nl>
2022-10-02 20:03:18 +0300

    Updates

---
+++ precache-manifest.a129b48b9f5962a5e5cf89858a07c6c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccc8d81f5ad91bdc3f186ec8b7415fc",
+    "url": "/e-origami-shop-app/index.html"
+  },
+  {
+    "revision": "61604854978531135abf",
+    "url": "/e-origami-shop-app/static/css/main.b66a6e3a.chunk.css"
+  },
+  {
+    "revision": "71040bdb8962affb0bf5",
+    "url": "/e-origami-shop-app/static/js/2.810dbea3.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/e-origami-shop-app/static/js/2.810dbea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36ca9af03bfaf0418e0",
+    "url": "/e-origami-shop-app/static/js/3.15c73d6a.chunk.js"
+  },
+  {
+    "revision": "61604854978531135abf",
+    "url": "/e-origami-shop-app/static/js/main.ce31d35b.chunk.js"
+  },
+  {
+    "revision": "6dee2b5f78708f449b78",
+    "url": "/e-origami-shop-app/static/js/runtime-main.3d8616fb.js"
+  },
+  {
+    "revision": "13d9e0a0d3947bb023c8b3ec37360019",
+    "url": "/e-origami-shop-app/static/media/bird.13d9e0a0.svg"
+  }
+]);
